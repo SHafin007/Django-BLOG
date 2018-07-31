ae6ee8dab0456aca9fe7a279c2ffc36b5b310f93
Habibur Rahman <opriyoshafin@gmail.com>
2018-07-31 17:05:58 +0600

    BLOG

--- blg/src/posts/views.py
+++ blg/src/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
@@ -11,14 +11,19 @@ def post_create(request):
 	}
 	return render(request,"index.html",context)
 
-def post_detail(request):
+def post_detail(request, id=None): #retrive
+	# instance = Post.objects.get(id=3)
+	instance = get_object_or_404(Post, id=id)
 	context={
-	"title":"Detail"
+	"title":instance.title,
+	"instance":instance,
 	}
-	return render(request,"index.html",context)
+	return render(request,"post_detail.html",context)
 
 def post_list(request):
+	queryset= Post.objects.all()
 	context={
+	"object_list":queryset,
 	"title":"List"
 	}
 	return render(request,"index.html",context)
@@ -33,4 +38,4 @@ def post_delete(request):
 	context={
 	"title":"Delete"
 	}
-	return render(reque
+	return render(request,"index.html",context)

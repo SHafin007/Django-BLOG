8a10b92fa4b9638dfe7cbde681223fdcd9675856
Habibur Rahman <opriyoshafin@gmail.com>
2018-08-06 03:45:43 +0600

    BLOG

--- blg/src/posts/models.py
+++ blg/src/posts/models.py
@@ -2,9 +2,20 @@ from django.db import models
 from django.core.urlresolvers import reverse
 # Create your models here.
 
+
+def upload_locations(instance,filename):
+	return "%s/%s" %(instance.id, filename)
+
+
 class Post (models.Model):
 
 	title 			=	models.CharField(max_length=120)
+	image 			=	models.ImageField( upload_to=upload_locations,
+											null=True, blank=True,
+											width_field="width_field",
+											height_field="height_field")
+	height_field	=	models.IntegerField(default=0)
+	width_field		=	models.IntegerField(default=0)
 	content			=	models.TextField()
 	updated			=	models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp		=	models.DateTimeField(auto_now=False, auto_now_add=True)
@@ -17,3 +28,7 @@ class Post (models.Model):
 	def get_absolute_url(self):
 		return reverse("posts:detail", kwargs={"id": self.id})
 		# return "/posts/%s/" %(self.id)
+
+
+	class Meta:
+		ordering =["-timestamp", "-updated"]
